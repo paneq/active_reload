113bef298494189a9047069121fe0fb709158eae
Robert Pankowecki (Gavdi) <rpa@gavdi.com>
2011-07-16 11:40:33 +0200

    dummy rails apps

---
+++ test/dummy309/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy309::Application.config.secret_token = '902766a1d9f631bce4d27ad72bf0b4e79704271e4929b30401371307cb76f5b1c402aa1ba2e0a0a8ae89b25d9d87c86443f60daa653ce0dd1fa9357e81a0bb38'
---
+++ test/dummy310rc4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy310rc4::Application.config.secret_token = '63f22d8dffc57bb0684cb6d95a5514d1e4d18b548a59b46e510da81e9296ce7ad80591d8acccb581df66fa80d4692fe51e34f4a387d9c43ad0825dc278be0fd4'
